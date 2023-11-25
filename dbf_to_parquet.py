f83b2b1acec03d35b4b82ed566f9143fd43b4bd3
Mostafa Hussain <shoman250@gmail.com>
2023-11-25 17:15:17 +0300

    adds simple conversion from dbf to csv

--- dbf_to_parquet.py
+++ dbf_to_parquet.py
@@ -1,1 +1,15 @@
 # author: mostafa
+
+# %%
+
+import pandas as pd
+from dbfread import DBF
+# %%
+table = DBF("D:\\projects\\mdq\\db\\output\\special_request\\New folder\\cat\\phrmtran.DBF")
+
+
+# %%
+df =  pd.DataFrame(table)
+
+# %%
+df.to_csv("D:\\projects\\mdq\\db\\output\\special_request\\New folder\\cat\\phrmtran_csv.csv")
