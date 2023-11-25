7b6cef9c13d163ad5e63e65b49fdbb54297dc360
Mahmoud Abdelrazek <razekmh@gmail.com>
2023-11-25 19:14:37 +0300

    changes output to depend on input

--- dbf_to_parquet.py
+++ dbf_to_parquet.py
@@ -7,25 +7,26 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-i", "--input", help="input dbf file path", default="D:\\projects\\mdq\\db\\output\\special_request\\New folder\\cat\\phrmtran.DBF")
-parser.add_argument("-o", "--output", help="output parquet file path", default="D:\\projects\\mdq\\db\\output\\special_request\\New folder\\cat\\phrmtran")
 parser.add_argument("-f", "--format", help="output format", default="parquet")
 args = parser.parse_args()
 
-table = DBF("D:\\projects\\mdq\\db\\output\\special_request\\New folder\\cat\\phrmtran.DBF")
+output_path = args.input.split(".")[0]
+
+table = DBF(args.input)
 df =  pd.DataFrame(table)
 
-def dbf_to_parquet_func(table, output_path):
+def dbf_to_parquet_func(output_path):
     output_path = pathlib.Path(output_path+".parquet")
     df.to_parquet(output_path)
 
-def dbf_to_csv_func(table, output_path):
+def dbf_to_csv_func(output_path):
     output_path = pathlib.Path(output_path+".csv")
     df.to_csv(output_path)
 
 if args.format == "parquet":
-    dbf_to_parquet_func(table, args.output)
+    dbf_to_parquet_func(table, output_path)
 elif args.format == "csv":
-    dbf_to_csv_func(table, args.output)
+    dbf_to_csv_func(table, output_path)
 else:
     print("invalid format")
 
