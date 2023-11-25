a203f244da2cecf3fd6052bb5b32cffa4361a6ae
Mahmoud Abdelrazek <razekmh@gmail.com>
2023-11-25 17:44:49 +0300

    adds option export to parquet

--- dbf_to_parquet.py
+++ dbf_to_parquet.py
@@ -10,16 +10,19 @@ import argparse
 # %%
 parser = argparse.ArgumentParser()
 parser.add_argument("-i", "--input", help="input dbf file path", default="D:\\projects\\mdq\\db\\output\\special_request\\New folder\\cat\\phrmtran.DBF")
-parser.add_argument("-o", "--output", help="output parquet file path", default="D:\\projects\\mdq\\db\\output\\special_request\\New folder\\cat\\phrmtran.parquet")
+parser.add_argument("-o", "--output", help="output parquet file path", default="D:\\projects\\mdq\\db\\output\\special_request\\New folder\\cat\\phrmtran")
 parser.add_argument("-f", "--format", help="output format", default="parquet")
 args = parser.parse_args()
 
 # %%
 table = DBF("D:\\projects\\mdq\\db\\output\\special_request\\New folder\\cat\\phrmtran.DBF")
 
-
-# %%
 df =  pd.DataFrame(table)
-
 # %%
-df.to_csv("D:\\projects\\mdq\\db\\output\\special_request\\New folder\\cat\\phrmtran_csv.csv")
+def dbf_to_parquet(table, output_path):
+    output_path = pathlib.Path(output_path+".parquet")
+    df.to_parquet(output_path)
+
+def dbf_to_csv(table, output_path):
+    output_path = pathlib.Path(output_path+".csv")
+    df.to_csv(output_path)
