ae22b043f319d658808531362b4612c99cf60aab
Mahmoud Abdelrazek <razekmh@gmail.com>
2023-11-25 18:31:54 +0300

    adds funcs for export

--- dbf_to_parquet.py
+++ dbf_to_parquet.py
@@ -1,28 +1,31 @@
 # author: mostafa
 
-# %%
-
 import pandas as pd
 from dbfread import DBF
 import pathlib
 import argparse
 
-# %%
 parser = argparse.ArgumentParser()
 parser.add_argument("-i", "--input", help="input dbf file path", default="D:\\projects\\mdq\\db\\output\\special_request\\New folder\\cat\\phrmtran.DBF")
 parser.add_argument("-o", "--output", help="output parquet file path", default="D:\\projects\\mdq\\db\\output\\special_request\\New folder\\cat\\phrmtran")
 parser.add_argument("-f", "--format", help="output format", default="parquet")
 args = parser.parse_args()
 
-# %%
 table = DBF("D:\\projects\\mdq\\db\\output\\special_request\\New folder\\cat\\phrmtran.DBF")
-
 df =  pd.DataFrame(table)
-# %%
-def dbf_to_parquet(table, output_path):
+
+def dbf_to_parquet_func(table, output_path):
     output_path = pathlib.Path(output_path+".parquet")
     df.to_parquet(output_path)
 
-def dbf_to_csv(table, output_path):
+def dbf_to_csv_func(table, output_path):
     output_path = pathlib.Path(output_path+".csv")
     df.to_csv(output_path)
+
+if args.format == "parquet":
+    dbf_to_parquet_func(table, args.output)
+elif args.format == "csv":
+    dbf_to_csv_func(table, args.output)
+else:
+    print("invalid format")
+
