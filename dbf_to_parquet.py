39834651847f753620d493e73b0c594cf3e83280
Mahmoud Abdelrazek <razekmh@gmail.com>
2023-11-25 17:42:16 +0300

    adds argparse base

--- dbf_to_parquet.py
+++ dbf_to_parquet.py
@@ -4,6 +4,16 @@
 
 import pandas as pd
 from dbfread import DBF
+import pathlib
+import argparse
+
+# %%
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", "--input", help="input dbf file path", default="D:\\projects\\mdq\\db\\output\\special_request\\New folder\\cat\\phrmtran.DBF")
+parser.add_argument("-o", "--output", help="output parquet file path", default="D:\\projects\\mdq\\db\\output\\special_request\\New folder\\cat\\phrmtran.parquet")
+parser.add_argument("-f", "--format", help="output format", default="parquet")
+args = parser.parse_args()
+
 # %%
 table = DBF("D:\\projects\\mdq\\db\\output\\special_request\\New folder\\cat\\phrmtran.DBF")
 
